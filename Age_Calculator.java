43efebdccb3cf888c2c335ff8037654242ecfa17
Abdelfateh Nadir Djelti <168342468+its-nadir@users.noreply.github.com>
2025-04-27 21:10:54 +0200

    Update Age_Calculator.java

--- Age_Calculator.java
+++ Age_Calculator.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
 class Test{
-    
     public static void main(String[] arg) {
         System.out.print("Enter Your Birth Year: ");
         int x=new Scanner(System.in).nextInt();
